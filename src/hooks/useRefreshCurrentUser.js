60b75017f2f4f2cfc94ef5b4b0262b4cc5abd125
Yurkes2020 <yurkes2020@gmail.com>
2023-01-08 13:17:26 +0200

    pack

--- src/hooks/useRefreshCurrentUser.js
+++ src/hooks/useRefreshCurrentUser.js
@@ -10,15 +10,11 @@ function useRefreshCurrentUser() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    // setCurrentUser(dispatch(operations.getCurrentUser()));
     dispatch(operations.getCurrentUser())
       .unwrap()
       .then(user => setCurrentUser(user))
       .catch(e => {
-        // fucking catch )))))
-        console.log(e);
       });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return { currentUser, isRefreshing };
 }
