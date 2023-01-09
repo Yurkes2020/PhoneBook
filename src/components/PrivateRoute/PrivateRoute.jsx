2b4dad005c383c21623f5c35a2ee585e905f0cab
Yurkes2020 <yurkes2020@gmail.com>
2023-01-09 16:07:51 +0200

    privete

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -2,9 +2,10 @@ import { Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { getIsLoggedIn } from 'redux/auth/auth-selectors';
 
-function PrivateRoute({ redirectTo = '/' }) {
+function PrivateRoute({ component: Component, redirectTo = '/' }) {
   const isLoggedIn = useSelector(state => getIsLoggedIn(state));
-  if (isLoggedIn) return <Navigate to={redirectTo} />;
+  return !isLoggedIn ? <Navigate to={redirectTo} /> : Component;
+  // if (!isLoggedIn) return <Navigate to={redirectTo} />;
 }
 
 export default PrivateRoute;
