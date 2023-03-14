7705e0bcbc7e88af7c214e0b072636f58edb38e4
Yurkes2020 <yurkes2020@gmail.com>
2023-03-14 17:12:03 +0200

    asd

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ root.render(
   <React.StrictMode>
     <PersistGate loading={<Loader />} persistor={persistor}>
       <Provider store={store}>
-        <BrowserRouter basename="/PhoneBook">
+        <BrowserRouter basename="/yurkes2020.github.io">
           <App />
         </BrowserRouter>
       </Provider>
